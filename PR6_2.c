e2c9f7f89a8ff58a655e63e4cc5f5035497f3695
prince-buha <150029430+prince-buha@users.noreply.github.com>
2023-12-25 11:20:23 +0530

    Update PR6_2.c

--- PR6_2.c
+++ PR6_2.c
@@ -1,3 +1,20 @@
+/*Q.2 Write a Program to count the frequency of each character in a given string.
+For example,
+Input:
+Enter any string: development
+
+Output:
+Frequency of each letter:
+d => 1
+e => 2
+v => 1
+l => 1
+o => 1
+p => 1
+m => 1
+n => 1
+t => 1
+	*/
 #include <stdio.h>
 #include <string.h>
 void main(){
